6fea1faa6e05a796ccec65e12762d2f5ec29b88d
TrustedMercury <kevin.jt2007@gmail.com>
2021-02-28 20:59:51 -0500

    Loaded plugins;

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
-import Pool from "pg-pool";
 import dotenv from "dotenv";
 import fastify from "fastify";
 
-import cors from "fastify-cors";
-import helmet from "fastify-helmet";
-import compression from "compression";
-import middleware from "fastify-express"
+import database from "./plugins/database.js";
+import middleware from "./plugins/middleware.js";
 
 // Loading environment variables
 dotenv.config();
@@ -22,34 +19,16 @@ const debug = (message) => {
   process.env.DEBUG_MODE && app.log.info(message)
 }
 
-// Create a PostgreSQL pool connection
-const pool = new Pool({
-  database: process.env.DATABASE_NAME,
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  password: process.env.DATABASE_PASS,
-  port: process.env.DATABASE_PORT,
-  idleTimeoutMillis: 1000,
-  _connectionTimeoutMillis: 1000
-});
-
 // Asynchronous tasks
-let database;
 (async () => {
 
-  // Register middleware
-  await app.register(middleware);
-  app.register(cors);
-  app.register(helmet);
-  app.use(compression);
-
-  // Connect to the database pool
-  database = await pool.connect();
+  // Initialize plugins
+  app.register(database);
+  app.register(middleware);
 
   // Run the webserver
-  await app.listen(process.env.PORT)
-  app.log.info(`Server Started on PORT ${process.env.PORT}`)
+  await app.listen(process.env.PORT);
 
 })();
 
-export { app, database, debug }
+export { app, debug }
